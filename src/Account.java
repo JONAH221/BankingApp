63e8e559612d382c3b1ab03b8bc0aab33b42d0ab
gafink01 <gafink01@wsc.edu>
2025-02-12 13:23:13 -0600

    Made the start of the id and the name strings o make it so you cant have no name so you can recieve and id.

--- src/Account.java
+++ src/Account.java
@@ -7,13 +7,30 @@ public class Account {
     private static int lastId = 0;
     private int id;
     private String firstName;
+    private String lastName;
 
 
 
-    public Account(String Name) {
-        lastId++;
-        this.id = lastId;
+
+    public Account(String firstName,String lastName) {
+        this.id = ++lastId;
         this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     * Setsetname
+     * set the last so you cant have a null name so it can assign you an id number
+     */
+    public void setlastName (String lastName) {
+        if (lastName == null || lastName.isEmpty())
+            throw new IllegalArgumentException("Invalid last name! Name can't be blank for id: " + id);
+        else
+            this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 
     public int getAccountId() {
