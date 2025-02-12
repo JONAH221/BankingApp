f7657f1df1f547587bc9ada151bb6fe4c80d7e08
JONAH221 <Jarvisface75@gmail.com>
2025-02-12 13:46:16 -0600

    called the Account.java file and tested it with names and attaching ids with names and outputting that to the console.

--- src/Tester.java
+++ src/Tester.java
@@ -3,6 +3,24 @@ public class Tester {
 
     public static void main(String[] args) {
 
+        /**
+         *  testing for the Account.java file in tester and testing inputs
+         *  creates new accounts with names. attach accountId to the name and displays it to the
+         *  console.
+         *
+         */
+        Account account1 = new Account("John", "Doe");
+        Account account2 = new Account("Jane", "Doe");
+
+
+        System.out.println("Account 1 Full Name: " + account1.getFullName());
+        System.out.println("Account 1 ID: " + account1.getAccountId());
+
+        System.out.println();
+
+        System.out.println("Account 2 Full Name: " + account2.getFullName());
+        System.out.println("Account 2 ID: " + account2.getAccountId());
+
 
 
     }
